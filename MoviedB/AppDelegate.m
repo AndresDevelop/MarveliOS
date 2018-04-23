3222374ded051d783d772c417b1b7bb831100cae
YebrailAndres <yebrail_andrs@hotmail.com>
2018-04-23 14:01:41 -0500

    Apk android

--- MoviedB/AppDelegate.m
+++ MoviedB/AppDelegate.m
@@ -1,9 +1,4 @@
-//
-//  AppDelegate.m
-//  MoviedB
-//
-//  Created by Yebrail andres Pacheco jaime on 23/11/17.
-//  Copyright © 2017 Yebrail Andres Pacheco jaime. All rights reserved.
+
 //
 
 #import "AppDelegate.h"
